2cb54d11579656eca1e53859bbd5ecb7800b450f
Marsha Battee <marsha@sanesuite.com>
2019-06-24 11:14:47 -0600

    Variables and Types

---
+++ variables-and-data.js
@@ -0,0 +1,44 @@
+/*
+* Variables, Data Types, and Typing
+ */
+let wordData;
+
+wordData = "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let y = subTotal * 0.07;
+let totalWithTax = y + subTotal;
+
+console.log(totalWithTax);
+
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "Internal Object Value",
+}
+};
+
+console.log(obj.obj2.obj2key1);
+
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+let add =  1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+console.log(mod);
+
+let string = "string 1 " + "string 2";
+
+console.log(string);
